b6acd796e2940c1326bebfd5067d5dbcf7aaa869
Anders Nilsson <anders.nilsson@aller.com>
2022-05-09 14:46:43 +0200

    Built files

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const octokit = github.getOctokit(GITHUB_TOKEN);
 const { context = {} } = github;
 const { pull_request } = context.payload;
 
-await octokit.issues.createComment({
+octokit.issues.createComment({
   ...context.repo,
   issue_number: pull_request.number,
   body: 'Manual merging is disabled. To start merging process use the slash command */merge-it* in a new comment. That will trigger testing pipeline and merging.'
