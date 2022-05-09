1db3d43d5517326a7fa5a56867f0f106cdf01a40
Anders Nilsson <anders.nilsson@aller.com>
2022-05-09 11:42:16 +0200

    Create comment on PR opened

---
+++ index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+const octokit = github.getOctokit(GITHUB_TOKEN);
+
+const { context = {} } = github;
+const { pull_request } = context.payload;
+
+await octokit.issues.createComment({
+  ...context.repo,
+  issue_number: pull_request.number,
+  body: 'Manual merging is disabled. To start merging process use the slash command */merge-it* in a new comment. That will trigger testing pipeline and merging.'
+});
