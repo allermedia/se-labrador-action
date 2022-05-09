f2b2cdaa541f35c6525b3f4466e7e2dbfc7719a8
Anders Nilsson <anders.nilsson@aller.com>
2022-05-09 16:45:53 +0200

    Version 1.21

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const octokit = github.getOctokit(GITHUB_TOKEN);
 const { context = {} } = github;
 const { pull_request } = context.payload;
 
-octokit.issues.createComment({
-  ...context.repo,
-  issue_number: pull_request.number,
-  body: 'Manual merging is disabled. To start merging process use the slash command */merge-it* in a new comment. That will trigger testing pipeline and merging.'
-});
+async function run() {
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: 'Manual merging is disabled. To start merging process use the slash command */merge-it* in a new comment. That will trigger testing pipeline and merging.'
+  });  
+}
+
+run();
