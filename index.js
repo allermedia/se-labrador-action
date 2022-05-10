801832e75c312a7a5d8e948d733d6a42394606cc
Anders Nilsson <anders.nilsson@aller.com>
2022-05-10 10:51:58 +0200

    Version 1.23

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const github = require('@actions/github');
 
 const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 const octokit = github.getOctokit(GITHUB_TOKEN);
-console.log(octokit);
 
 const { context = {} } = github;
 const { pull_request } = context.payload;
 
 async function run() {
-  await octokit.issues.createComment({
+  await octokit.rest.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
     body: 'Manual merging is disabled. To start merging process use the slash command */merge-it* in a new comment. That will trigger testing pipeline and merging.'
